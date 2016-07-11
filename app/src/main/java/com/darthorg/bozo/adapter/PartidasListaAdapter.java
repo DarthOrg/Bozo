c7c66b9b45fc933bb4cb6b3ae9a13bcaf6caace8
Gustavo Candido de Oliveira <gustavo.gcdo@gmail.com>
2016-07-11 19:20:27 -0400

    Botao deletar partida

--- app/src/main/java/com/darthorg/bozo/adapter/PartidasListaAdapter.java
+++ app/src/main/java/com/darthorg/bozo/adapter/PartidasListaAdapter.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.darthorg.bozo.R;
+import com.darthorg.bozo.dao.PartidaDAO;
 import com.darthorg.bozo.model.Partida;
 
 import java.util.List;
@@ -43,12 +46,24 @@ public class PartidasListaAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View view, ViewGroup viewGroup) {
+    public View getView(final int position, View view, ViewGroup viewGroup) {
         View v = View.inflate(mContext, R.layout.theme_listview_partidas, null);
         TextView tvNumero = (TextView) v.findViewById(R.id.txt_lista_numero);
         TextView tvNomePartida = (TextView) v.findViewById(R.id.txt_lista_nome_partida);
 
-        tvNumero.setText(String.valueOf(mPartidasLista.get(position).getIdPartida()));
+        Button btn = (Button) v.findViewById(R.id.btnDeletarPartida);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext, "Deletou", Toast.LENGTH_SHORT).show();
+                PartidaDAO partidaDAO = new PartidaDAO(mContext);
+                partidaDAO.deletarPartida(mPartidasLista.get(position));
+                mPartidasLista.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+
+        tvNumero.setText(String.valueOf(position + 1));
         tvNomePartida.setText(mPartidasLista.get(position).getNome());
 
         v.setTag(mPartidasLista.get(position).getIdPartida());
