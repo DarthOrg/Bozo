79706883f65806d0afdc8cce66e1646399c34ea2
Wendell Ugalds <wendellugaldsg@gmail.com>
2016-07-26 20:25:33 -0400

    Modificados alguns layouts

--- app/src/main/java/com/darthorg/bozo/view/Inicio.java
+++ app/src/main/java/com/darthorg/bozo/view/Inicio.java
@@ -1,6 +1,8 @@
 package com.darthorg.bozo.view;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,9 +16,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import com.darthorg.bozo.R;
+import com.darthorg.bozo.controller.JogadorController;
+import com.darthorg.bozo.fragment.FragmentFilho;
+import com.darthorg.bozo.model.Jogador;
+import com.github.clans.fab.FloatingActionMenu;
 
 public class Inicio extends AppCompatActivity {
 
@@ -47,7 +55,7 @@ public class Inicio extends AppCompatActivity {
                 //Recebe a activity para persolnalizar o dialog
                 View dialogLayout = inflater.inflate(R.layout.dialog_inicio, null);
                 //Botão nova partida
-                ImageButton btnNovaPartida = (ImageButton) dialogLayout.findViewById(R.id.btn_nova_partida);
+                Button btnNovaPartida = (Button) dialogLayout.findViewById(R.id.btn_nova_partida);
                 btnNovaPartida.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -69,8 +77,8 @@ public class Inicio extends AppCompatActivity {
                 btnConfiguracoes.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Snackbar.make(view, "Configurações clicado.", Snackbar.LENGTH_LONG)
-                                .setAction("Ok", null).show();
+
+                        return;
                     }
                 });
                 //Botão Instruções
@@ -78,8 +86,9 @@ public class Inicio extends AppCompatActivity {
                 btnInstrucoes.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Snackbar.make(view, "Instruções clicado.", Snackbar.LENGTH_LONG)
-                                .setAction("Ok", null).show();
+
+
+                        return;
                     }
                 });
                 //Botão Sobre
@@ -87,8 +96,8 @@ public class Inicio extends AppCompatActivity {
                 btnSobre.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        Snackbar.make(view, "Configurações clicado.", Snackbar.LENGTH_LONG)
-                                .setAction("Ok", null).show();
+
+                        return;
                     }
                 });
 
@@ -98,25 +107,6 @@ public class Inicio extends AppCompatActivity {
                 builder.show();
             }
         });
-
-
-        /*//Botão de informção do dado (Ás(01))
-        btnInfoAs = (Button) findViewById(R.id.btn_info_as);
-        btnInfoAs.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(Inicio.this);
-                builder.setTitle("ÁZ ou ( Número 01 )");
-                builder.setMessage("[ Áz ] é uma pedra de menor valor no Bozó. " +
-                        " Sua posição fica no canto superior á esquerda," +
-                        " soma-se os valores da pedra de áz." +
-                        " sua pontuação varia de 1 à 5 pontos");
-                builder.setIcon(R.drawable.ic_um);
-                builder.setNegativeButton("instruções",null);
-                builder.setPositiveButton("Entendi",null);
-                builder.show();
-            }
-        });*/
 
     }
 
