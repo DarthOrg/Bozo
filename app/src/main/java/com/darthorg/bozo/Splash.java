8da2a8b53339eafd5a2cdfbf91b39d1d87a59bd1
Wendell Ugalds <wendellugaldsg@gmail.com>
2016-08-17 02:10:37 -0400

    cor splash

--- app/src/main/java/com/darthorg/bozo/Splash.java
+++ app/src/main/java/com/darthorg/bozo/Splash.java
@@ -16,10 +16,6 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT > 16) {
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        }
         setContentView(R.layout.activity_splash);
 
 
