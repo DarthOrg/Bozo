6aea9553c69c4778f30ae77eb30a0dbb47033873
Wendell Ugalds <wendellugaldsg@gmail.com>
2016-08-24 05:30:41 -0400

    Tentando melhorar o app

--- app/src/main/java/com/darthorg/bozo/Splash.java
+++ app/src/main/java/com/darthorg/bozo/Splash.java
@@ -1,11 +1,9 @@
 package com.darthorg.bozo;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.WindowManager;
 
 import com.darthorg.bozo.view.Inicio;
 
--- app/src/main/java/com/darthorg/bozo/view/Inicio.java
+++ app/src/main/java/com/darthorg/bozo/view/Inicio.java
@@ -1,92 +1,165 @@
 package com.darthorg.bozo.view;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.ContextMenu;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.darthorg.bozo.R;
+import com.darthorg.bozo.adapter.PartidasListaAdapter;
+import com.darthorg.bozo.dao.PartidaDAO;
+import com.darthorg.bozo.model.Partida;
 
-public class Inicio extends AppCompatActivity {
+import java.util.List;
+
+public class Inicio extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
 
-    private Toolbar toolbar;
-    Button btnGruposSalvos,btnConfiguracoes, btnInstrucoes, btnInstrucoesDois;
-    ImageButton btnNovaPartida, menu,sair;
-    ImageView imgTopo, imgBottom;
-    TextView btnSobre,tituloInfo,textoInfo;
-    RelativeLayout botaoAz,botaoDuque,botaoTerno,botaoQuadra,botaoQuina,botaoSena,conteudoInfo,textoBozo;
-    LinearLayout qualquer;
-    private int mId;
+    private ListView listViewPartidas;
+    private PartidasListaAdapter partidasListAdapter;
+    private PartidaDAO partidaDAO;
+    private List<Partida> partidaList;
+    Toolbar toolbar;
+    protected SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
-    protected void onCreate(final Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.TextoVazio);
+
+        toolbar.setTitle(R.string.GruposDeJogo);
+
+
         setSupportActionBar(toolbar);
 
-        //Botao Novo Grupo
-        btnNovaPartida = (ImageButton) findViewById(R.id.btnNovaPartida);
-        btnNovaPartida.setOnClickListener(new View.OnClickListener() {
+        listViewPartidas = (ListView) findViewById(R.id.list_view_partidas);
+
+        //Aparecer imagem quando a lista estiver vazia
+        listViewPartidas.setEmptyView(findViewById(R.id.list_vazio));
+
+        //botão ficar precionado
+        registerForContextMenu(listViewPartidas);
+
+
+        partidaDAO = new PartidaDAO(this);
+        partidaList = partidaDAO.buscarPartidas();
+
+
+        partidasListAdapter = new PartidasListaAdapter(getApplicationContext(), partidaList);
+        listViewPartidas.setAdapter(partidasListAdapter);
+
+//botão ficar precionado
+        registerForContextMenu(listViewPartidas);
+
+        listViewPartidas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(Inicio.this, PartidaAberta.class);
+                intent.putExtra("partidaSalva", partidaList.get(position).getIdPartida());
+                intent.putExtra("partidaNova", false);
+                startActivity(intent);
+
+                finish();
+            }
+        });
+
+        //atualizar
+        mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.stl_swipe);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+               new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        SystemClock.sleep(2000);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                partidaDAO = new PartidaDAO(Inicio.this);
+                                partidaList = partidaDAO.buscarPartidas();
+                                partidasListAdapter = new PartidasListaAdapter(getApplicationContext(), partidaList);
+                                listViewPartidas.setAdapter(partidasListAdapter);
+                                mSwipeRefreshLayout.setRefreshing(false);
+                            }
+                        });
+                    }
+                }).start();
+            }
+        });
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Inicio.this, NovaPartida.class);
-                    Inicio.this.startActivity(intent);
-
-
-
+                Inicio.this.startActivity(intent);
             }
         });
 
-        btnSobre = (TextView) findViewById(R.id.btnSobre);
-        btnSobre.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Inicio.this, Sobre.class);
-                startActivity(intent);
-            }
-        });
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
 
-        //Fab Grupos Salvos
-        btnGruposSalvos = (Button) findViewById(R.id.btnGruposSalvos);
-        btnGruposSalvos.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Inicio.this, ListaDePartidas.class);
-                startActivity(intent);
-            }
-        });
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
 
-        //Fab Configurações
-        btnConfiguracoes = (Button) findViewById(R.id.btnConfiguracoes);
-        btnConfiguracoes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                return;
-            }
-        });
-
-
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.popup_menu, menu);
     }
 
 
-    //Botão voltar
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.action_apagar:
+
+                int position = 0;
+                Toast.makeText(Inicio.this, partidaList.get(position).getNome() + " - foi excluido da sua lista", Toast.LENGTH_SHORT).show();
+                PartidaDAO partidaDAO = new PartidaDAO(Inicio.this);
+                partidaDAO.deletarPartida(partidaList.get(position));
+                partidaList.remove(position);
+                partidasListAdapter.notifyDataSetChanged();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+
+        }
+
+    }
+
     @Override
     public void onBackPressed() {
-
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -97,7 +170,6 @@ public class Inicio extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         int id = item.getItemId();
 
         if (id == R.id.action_bloquear_som) {
@@ -107,5 +179,31 @@ public class Inicio extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        if (id == R.id.nav_novo_grupo) {
+            Intent intent = new Intent(Inicio.this, NovaPartida.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_instrucoes) {
+
+        } else if (id == R.id.nav_cor_destaque) {
+
+        } else if (id == R.id.nav_sobre) {
+            Intent intent = new Intent(Inicio.this, Sobre.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_compartilhar) {
+
+        } else if (id == R.id.nav_configuracoes) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
