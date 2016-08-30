00f01b1d31bf58273edd1610ad8730debeb85e41
Gustavo Candido de Oliveira <gustavo.gcdo@gmail.com>
2016-08-30 15:52:31 -0400

    Mudando a maneira em que uma partida é salva

--- app/src/main/java/com/darthorg/bozo/controller/PartidaController.java
+++ app/src/main/java/com/darthorg/bozo/controller/PartidaController.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.darthorg.bozo.dao.PartidaDAO;
 import com.darthorg.bozo.model.Jogador;
 import com.darthorg.bozo.model.Partida;
+import com.darthorg.bozo.model.Rodada;
 
 import java.util.List;
 
@@ -16,17 +17,27 @@ public class PartidaController {
 
     private Context context;
     private PartidaDAO partidaDAO;
+    private RodadaController rodadaController;
 
     public PartidaController(Context context) {
         this.context = context;
         partidaDAO = new PartidaDAO(context);
+        rodadaController = new RodadaController(context);
 
     }
 
     //TODO: Nao deixar inserir partidas com nomes iguais
     public long inserirPartida(Partida partida) {
         if (partida.getNome() != null) {
-            return partidaDAO.novaPartida(partida);
+
+            long idPartida = partidaDAO.novaPartida(partida);
+            // Insere as Rodadas Junto
+            for (int i = 0; i < partida.getRodadas().size(); i++) {
+                partida.getRodadas().get(i).setIdPartida(idPartida);
+                rodadaController.inserirRodada(partida.getRodadas().get(i));
+            }
+
+            return idPartida;
         } else {
             return -1;
         }
@@ -42,8 +53,12 @@ public class PartidaController {
         return partidaAtual;
     }
 
-    public List<Jogador> buscarJogadoresPartida(long idPartida){
+    public List<Jogador> buscarJogadoresPartida(long idPartida) {
         return partidaDAO.buscarJogadoresPartida(idPartida);
+    }
+
+    public List<Rodada> buscarRodadasPartida(long idPartida) {
+        return partidaDAO.buscarRodadasPartida(idPartida);
     }
 
 }
--- app/src/main/java/com/darthorg/bozo/controller/RodadaController.java
+++ app/src/main/java/com/darthorg/bozo/controller/RodadaController.java
@@ -3,7 +3,10 @@ package com.darthorg.bozo.controller;
 import android.content.Context;
 
 import com.darthorg.bozo.dao.RodadaDAO;
+import com.darthorg.bozo.model.Jogador;
 import com.darthorg.bozo.model.Rodada;
+
+import java.util.List;
 
 /**
  * Created by Gustavo on 11/07/2016.
@@ -12,17 +15,27 @@ public class RodadaController {
 
     private Context context;
     private RodadaDAO rodadaDAO;
+    private JogadorController jogadorController;
 
     public RodadaController(Context context) {
         this.context = context;
         rodadaDAO = new RodadaDAO(context);
+        jogadorController = new JogadorController(context);
 
     }
 
     //TODO: Nao deixar inserir rodadas que não tenham partida
     public long inserirRodada(Rodada rodada) {
         if (rodada.getIdPartida() != 0) {
-            return rodadaDAO.novaRodada(rodada);
+
+            long idRodada = rodadaDAO.novaRodada(rodada);
+
+            for (int i = 0; i < rodada.getJogadores().size(); i++) {
+                rodada.getJogadores().get(i).setIdRodada(idRodada);
+                jogadorController.inserirJogador(rodada.getJogadores().get(i));
+            }
+
+            return idRodada;
         } else {
             return -1;
         }
@@ -34,5 +47,9 @@ public class RodadaController {
 
         rodada = rodadaDAO.buscarRodadaPorPartida(idPartida);
         return rodada;
+    }
+
+    public List<Jogador> buscarJogadoresRodada(long idRodada) {
+        return rodadaDAO.buscarJogadoresRodada(idRodada);
     }
 }
