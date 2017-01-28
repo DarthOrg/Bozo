57bfce341d4819aa494df4e566f397d27a502433
Gustavo Candido de Oliveira <gustavo.gcdo@gmail.com>
2017-01-28 16:16:48 -0300

    Trocando de cor da tela splash

--- app/src/main/java/com/darthorg/bozo/Splash.java
+++ app/src/main/java/com/darthorg/bozo/Splash.java
@@ -32,7 +32,7 @@ public class Splash extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(getResources().getColor(R.color.colorAccentDarkWhite));
+            window.setStatusBarColor(getResources().getColor(R.color.colorAccentDark));
         }
     }
 }
