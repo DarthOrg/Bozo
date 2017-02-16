b996bf0b3d67874c687b40dae2ff1f5f6ed96cf9
Wendell Ugalds <wendellugaldsg@gmail.com>
2017-02-16 19:36:30 -0300

    atualizaão do layout na tela copo vitual

--- app/src/main/java/com/darthorg/bozo/view/CopoVirtual.java
+++ app/src/main/java/com/darthorg/bozo/view/CopoVirtual.java
@@ -3,6 +3,8 @@ package com.darthorg.bozo.view;
 import android.content.ClipData;
 import android.content.ClipDescription;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,11 +12,14 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.DragEvent;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
@@ -38,19 +43,25 @@ public class CopoVirtual extends AppCompatActivity {
     SensorManager sensorManager;
     Sensor sensorEmAcao;
 
-    LinearLayout linearLayout, dadosEmbaixo,receberDados;
-    RelativeLayout RLjogar, RLEmbaixo;
+    ImageButton dadoEncima1,dadoEncima2,dadoEncima3,dadoEncima4,dadoEncima5;
+
+    LinearLayout linearLayout, receberDados,dadosEncima,RLOlharEncima;
+    RelativeLayout RLjogar, RLEmbaixo,RLAtualizar;
     ImageView copo,sombra;
-    CardView caixaMenssagem;
-    TextView menssagem;
+    FloatingActionButton jogar,embaixo,atualizar,olhar;
+    Toolbar toolbar;
+    TextView txtMenssagem;
+    private int chances = 3;
+    private int btnEmbaixo = 0;
+    private int btnolhar = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_copo_virtual);
         fullscreenTransparent();
+        IDs();
 
-        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         toolbar.setNavigationIcon(getResources().getDrawable(R.drawable.ic_sair_white));
@@ -71,18 +82,7 @@ public class CopoVirtual extends AppCompatActivity {
         findViewById(R.id.lugar5).setOnDragListener(new MyOnDragListener(5));
         findViewById(R.id.llAreaPrincipal).setOnDragListener(new MyOnDragListener(6));
 
-//        final FloatingActionButton atualizar = (FloatingActionButton) findViewById(R.id.atualizar);
-        final FloatingActionButton jogar = (FloatingActionButton) findViewById(R.id.jogarDados);
-        final FloatingActionButton embaixo = (FloatingActionButton) findViewById(R.id.pedir_embaixo);
 
-        receberDados = (LinearLayout) findViewById(R.id.recebeDados);
-        caixaMenssagem = (CardView) findViewById(R.id.caixaMenssagem);
-        menssagem = (TextView) findViewById(R.id.menssagem);
-        RLjogar = (RelativeLayout) findViewById(R.id.rl_btnJogar);
-        RLEmbaixo = (RelativeLayout) findViewById(R.id.rl_btnEmbaixo);
-
-        copo = (ImageView) findViewById(R.id.copo);
-        sombra = (ImageView) findViewById(R.id.sombra);
 
         jogar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -90,13 +90,16 @@ public class CopoVirtual extends AppCompatActivity {
                 linearLayout = (LinearLayout) findViewById(R.id.llAreaPrincipal);
 
                 sensorManager.registerListener(new MySensorEvent(linearLayout), sensorEmAcao, SensorManager.SENSOR_DELAY_GAME);
-
                 linearLayout.setVisibility(View.GONE);
                 copo.setVisibility(View.VISIBLE);
                 sombra.setVisibility(View.VISIBLE);
                 RLjogar.setVisibility(View.GONE);
                 RLEmbaixo.setVisibility(View.VISIBLE);
-                menssagem.setText("\nCHACOALHE O CELULAR\n\nPara jogar os dados vire o celular de cabeça para baixo, caso queira 'EMBAIXO' clique na seta para baixo.\n");
+                RLOlharEncima.setVisibility(View.GONE);
+                dadosEncima.setVisibility(View.GONE);
+                txtMenssagem.setText("Chacoalhe o celular.");
+                btnEmbaixo = 0;
+
 
             }
         });
@@ -104,12 +107,84 @@ public class CopoVirtual extends AppCompatActivity {
         embaixo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Embaixo ATIVADO joque os dados",Toast.LENGTH_LONG).show();
+                btnEmbaixo ++;
+                switch (btnEmbaixo){
+                    case 1:
+                        txtMenssagem.setText("Embaixo ATIVO");
+                        break;
+                    case 2:
+                        txtMenssagem.setText("Embaixo DESATIVADO");
+                        btnEmbaixo = 0;
+                        break;
+                }
+            }
+        });
+
+        atualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                Intent intent = new Intent(CopoVirtual.this,CopoVirtual.class);
+                startActivity(intent);
+            }
+        });
+
+        olhar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnolhar ++;
+                switch (btnolhar){
+                    case 1:
+                        dadosEncima.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        dadosEncima.setVisibility(View.GONE);
+                        btnolhar = 0;
+                        break;
+                }
             }
         });
 
 
+
     }
+
+    public void IDs(){
+        //Toobar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        //FAB
+        jogar = (FloatingActionButton) findViewById(R.id.jogarDados);
+        embaixo = (FloatingActionButton) findViewById(R.id.pedir_embaixo);
+        atualizar = (FloatingActionButton) findViewById(R.id.atualizar);
+        olhar = (FloatingActionButton) findViewById(R.id.olhar);
+
+        //LinearLayout
+        receberDados = (LinearLayout) findViewById(R.id.recebeDados);
+        dadosEncima = (LinearLayout) findViewById(R.id.llDadosEncima);
+        RLOlharEncima = (LinearLayout) findViewById(R.id.rl_btnOlhar);
+
+        //RelativeLayout
+        RLjogar = (RelativeLayout) findViewById(R.id.rl_btnJogar);
+        RLEmbaixo = (RelativeLayout) findViewById(R.id.rl_btnEmbaixo);
+        RLAtualizar = (RelativeLayout) findViewById(R.id.rl_btnAtualizar);
+
+        //ImageView
+        copo = (ImageView) findViewById(R.id.copo);
+        sombra = (ImageView) findViewById(R.id.sombra);
+
+        //Texto
+        txtMenssagem =(TextView) findViewById(R.id.txtMenssagem);
+
+        //Dados Encima
+        dadoEncima1 = (ImageButton) findViewById(R.id.dadoEncima1);
+        dadoEncima2 = (ImageButton) findViewById(R.id.dadoEncima2);
+        dadoEncima3 = (ImageButton) findViewById(R.id.dadoEncima3);
+        dadoEncima4 = (ImageButton) findViewById(R.id.dadoEncima4);
+        dadoEncima5 = (ImageButton) findViewById(R.id.dadoEncima5);
+
+    }
+
 
 
     @Override
@@ -117,7 +192,7 @@ public class CopoVirtual extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == android.R.id.home) {
-            finish();
+            opçãoSair();
         }
 
         return super.onOptionsItemSelected(item);
@@ -179,6 +254,8 @@ public class CopoVirtual extends AppCompatActivity {
                     break;
             }
         }
+
+
     }
 
     class MySensorEvent implements SensorEventListener {
@@ -200,10 +277,36 @@ public class CopoVirtual extends AppCompatActivity {
                 linearLayout.setVisibility(View.VISIBLE);
                 copo.setVisibility(View.GONE);
                 sombra.setVisibility(View.GONE);
-                caixaMenssagem.setVisibility(View.GONE);
                 receberDados.setVisibility(View.VISIBLE);
                 RLjogar.setVisibility(View.VISIBLE);
                 RLEmbaixo.setVisibility(View.GONE);
+
+                chances --;
+                switch (chances){
+                    case 2:
+                        txtMenssagem.setText("Você tem mais "+chances+" jogadas");
+                        break;
+                    case 1:
+                        txtMenssagem.setText("Você tem só mais "+chances+" jogada");
+                        break;
+                    case 0:
+                        txtMenssagem.setText("SEGUIDA DE BOCA");
+                        RLjogar.setVisibility(View.GONE);
+                        RLAtualizar.setVisibility(View.VISIBLE);
+                        break;
+                }
+
+                switch (btnEmbaixo){
+                    case 1:
+                        RLOlharEncima.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        RLOlharEncima.setVisibility(View.GONE);
+                        btnEmbaixo = 0;
+                        break;
+                }
+
+
             }
         }
 
@@ -289,5 +392,53 @@ public class CopoVirtual extends AppCompatActivity {
             return false;
         }
 
+    }
+    public void AlertaSair(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.sair));
+        builder.setMessage("Você não terminou sua jogada, deseja sair assim mesmo?")
+                .setPositiveButton(getString(R.string.sair), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.show();
+    }
+
+    public void opçãoSair(){
+        if (chances == 3){
+            finish();
+        }else if (chances  == 2){
+            AlertaSair();
+        }else if (chances  == 1){
+            AlertaSair();
+        }else if (chances  == 0){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Lembrete");
+            builder.setMessage("Antes de sair sua jogada foi\n"+txtMenssagem.getText().toString())
+                    .setPositiveButton(getString(R.string.sair), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            finish();
+                            dialog.dismiss();
+                        }
+                    })
+                    .setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    });
+            builder.show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        opçãoSair();
     }
 }
