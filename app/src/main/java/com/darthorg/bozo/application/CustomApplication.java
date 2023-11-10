6bf39e8f87a7088b17c5dd0541c58e2e8bf6d3f8
Gustavo Oliveira <gustavo.gcdo@gmail.com>
2023-11-10 14:07:25 -0400

    feat: add onesignal novamente e limpando dependencias nao utilizadas

--- app/src/main/java/com/darthorg/bozo/application/CustomApplication.java
+++ app/src/main/java/com/darthorg/bozo/application/CustomApplication.java
@@ -1,17 +1,27 @@
 package com.darthorg.bozo.application;
 
+import static kotlinx.coroutines.CoroutineScopeKt.CoroutineScope;
+
 import android.app.Application;
 
-//import com.onesignal.OneSignal;
+import com.onesignal.Continue;
+import com.onesignal.OneSignal;
+import com.onesignal.debug.LogLevel;
 
 /**
  * Created by gusta on 05/03/2017.
  */
 
 public class CustomApplication extends Application {
+    private static final String ONESIGNAL_APP_ID = "0832ba70-d4e7-490f-a5da-30ad9f83fb63";
+
     @Override
     public void onCreate() {
         super.onCreate();
-//        OneSignal.startInit(this).init();
+
+        // Verbose Logging set to help debug issues, remove before releasing your app.
+        OneSignal.getDebug().setLogLevel(LogLevel.VERBOSE);
+        // OneSignal Initialization
+        OneSignal.initWithContext(this, ONESIGNAL_APP_ID);
     }
 }
